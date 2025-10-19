ef5988281d4ac23373a0781d14d560b1440fac28
hj-lab <heeju03180@naver.com>
2025-10-19 16:29:40 +0900

    feat: Add mailerConfig

---
+++ src/config/mailer.config.ts
@@ -0,0 +1,26 @@
+import { MailerAsyncOptions } from '@nestjs-modules/mailer/dist/interfaces/mailer-async-options.interface';
+import { EjsAdapter } from '@nestjs-modules/mailer/dist/adapters/ejs.adapter';
+import * as path from 'path';
+
+export class mailerConfig implements MailerAsyncOptions {
+  useFactory = () => ({
+    transport: {
+      host: process.env.SMTP_HOST,
+      port: process.env.SMTP_PORT,
+      auth: {
+        user: process.env.SMTP_EMAIL,
+        pass: process.env.SMTP_PASS,
+      },
+    },
+    defaults: {
+      from: '"AppleTree" <bookWheel@gmail.com>',
+    },
+    template: {
+      dir: path.join(__dirname, '/../auth/templates'),
+      adapter: new EjsAdapter(),
+      options: {
+        strict: false,
+      },
+    },
+  });
+}
