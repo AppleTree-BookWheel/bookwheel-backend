5554e31efa281169dc11cc8d74264402768a2e2f
hj-lab <heeju03180@naver.com>
2025-10-19 16:34:42 +0900

    feat: Add email verification test codes

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { mailerConfig } from 'src/config/mailer.config';
 
 @Module({
+  imports: [MailerModule.forRootAsync(new mailerConfig())],
   controllers: [AuthController],
   providers: [AuthService],
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,17 @@
+import { MailerService } from '@nestjs-modules/mailer';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly mailerService: MailerService) {}
+
+  public async sendVerificationEmail() {
+    const verificationCode = '123456';
+    await this.mailerService.sendMail({
+      to: 'heeju03180@naver.com',
+      subject: '책바퀴 이메일 인증 코드',
+      template: './verification',
+      context: { verificationCode },
+    });
+  }
+}
