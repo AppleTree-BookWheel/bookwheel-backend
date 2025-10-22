0688228f608ab31e9f6d09a0a5f696e205783bc4
hj-lab <heeju03180@naver.com>
2025-10-22 20:51:42 +0900

    feat: Add userModule, controller, service, repository

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,6 +9,7 @@ import { ClsModule } from 'nestjs-cls';
 import { ClsPluginTransactional } from '@nestjs-cls/transactional';
 import { TransactionalAdapterPrisma } from '@nestjs-cls/transactional-adapter-prisma';
 import { PrismaService } from '../prisma/prisma.service';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -27,6 +28,7 @@ import { PrismaService } from '../prisma/prisma.service';
         }),
       ],
     }),
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
