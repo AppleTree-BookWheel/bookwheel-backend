8695dfab143fb4ac7a8bc389d36bb6231e88ec7f
hj-lab <heeju03180@naver.com>
2025-10-23 15:41:09 +0900

    feat: Add selectBasicUserById in repo

--- src/user/user.repository.ts
+++ src/user/user.repository.ts
@@ -1,10 +1,24 @@
 import { TransactionHost } from '@nestjs-cls/transactional';
 import { TransactionalAdapterPrisma } from '@nestjs-cls/transactional-adapter-prisma';
 import { Injectable } from '@nestjs/common';
+import { SELECT_USER, SelectUser } from './model/prisma-type/select-user';
 
 @Injectable()
 export class UserRepository {
   constructor(
     private readonly txHost: TransactionHost<TransactionalAdapterPrisma>,
   ) {}
+
+  public async selectBasicUserById(id: string): Promise<SelectUser | null> {
+    return await this.txHost.tx.user.findFirst({
+      ...SELECT_USER,
+      where: {
+        basicAuths: {
+          is: {
+            id: id,
+          },
+        },
+      },
+    });
+  }
 }
