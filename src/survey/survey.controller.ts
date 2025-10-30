f8e55334e7eb4eca8c3465daf1e2953d830f7ae1
hj-lab <heeju03180@naver.com>
2025-10-30 17:36:03 +0900

    feat: Add getSurveyResponseByQuestionIdx in controller

--- src/survey/survey.controller.ts
+++ src/survey/survey.controller.ts
@@ -1,7 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param } from '@nestjs/common';
 import { SurveyService } from './survey.service';
+import { GetSurveyResponseDto } from './dto/response/get-survey-response.dto';
 
 @Controller('survey')
 export class SurveyController {
   constructor(private readonly surveyService: SurveyService) {}
+
+  // TODO : 토큰 구현후 userIdx 받는 방식 변경
+
+  @Get('/response')
+  async getSurveyResponseByQuestionIdx(
+    @Param('idx') idx: number,
+    @Body('questionIdx') questionIdx: number,
+  ): Promise<GetSurveyResponseDto | null> {
+    return this.surveyService.getSurveyResponseByQuestionIdx(idx, questionIdx);
+  }
 }
