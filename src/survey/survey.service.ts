8db142d539688526deacb55647f233cb5588d00c
hj-lab <heeju03180@naver.com>
2025-10-30 17:33:37 +0900

    feat: Implement survey CRUD methods in service

--- src/survey/survey.service.ts
+++ src/survey/survey.service.ts
@@ -1,4 +1,40 @@
 import { Injectable } from '@nestjs/common';
+import { SurveyRepository } from './survey.repository';
+import { CreateSurveyResponseInput } from './inputs/create-survey-response.input';
+import { UpdateSurveyResponseInput } from './inputs/update-survey-response.input';
+import { SurveyResponseModel } from './model/survey-response.model';
 
 @Injectable()
-export class SurveyService {}
+export class SurveyService {
+  constructor(private readonly surveyRepository: SurveyRepository) {}
+
+  public async getSurveyResponseByQuestionIdx(
+    idx: number,
+    questionIdx: number,
+  ): Promise<SurveyResponseModel | null> {
+    return this.surveyRepository
+      .selectSurveyResponseByQuestionIdx(idx, questionIdx)
+      .then(SurveyResponseModel.fromPrisma);
+  }
+
+  public async createSurveyResponse(
+    idx: number,
+    input: CreateSurveyResponseInput,
+  ): Promise<void> {
+    return this.surveyRepository.insertSurveyResponse(idx, input);
+  }
+
+  public async updateSurveyResponse(
+    idx: number,
+    input: UpdateSurveyResponseInput,
+  ): Promise<void> {
+    return this.surveyRepository.updateSurveyResponse(idx, input);
+  }
+
+  public async deleteSurveyResponse(
+    idx: number,
+    questionIdx: number,
+  ): Promise<void> {
+    return this.surveyRepository.deleteSurveyResponse(idx, questionIdx);
+  }
+}
