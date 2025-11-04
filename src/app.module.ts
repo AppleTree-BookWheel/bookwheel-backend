b5898088bfd176e3ec94788928835476cbbbab76
hj-lab <heeju03180@naver.com>
2025-11-04 22:03:07 +0900

    refactor: Remove unintended AuthModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from 'prisma/prisma.module';
-import { AuthModule } from './auth/auth.module';
 import { RedisModule } from './redis/redis.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     PrismaModule,
-    AuthModule,
     RedisModule,
   ],
   controllers: [AppController],
