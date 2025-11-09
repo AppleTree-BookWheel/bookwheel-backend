c59c8287f865507bc3bacd9f23467a52dc2e2cfe
hj-lab <heeju03180@naver.com>
2025-11-09 17:10:34 +0900

    feat: Add getUser method in controller and service

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Get('id/:id')
+  async getUserById(@Param('id') id: string) {
+    return this.userService.getUserById(id);
+  }
+
+  @Get('/:idx')
+  async getUserByIdx(@Param('idx', ParseIntPipe) idx: number) {
+    return this.userService.getUserByIdx(idx);
+  }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { UserRepository } from './user.repository';
+import { UserModel } from './model/user.model';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private readonly userRepository: UserRepository) {}
+
+  // id 중복 검사를 위한 메서드
+  public async getUserById(id: string) {
+    return this.userRepository.selectUserById(id);
+  }
+
+  public async getUserByIdx(idx: number): Promise<UserModel> {
+    const user = await this.userRepository.selectUserByIdx(idx);
+
+    if (!user) {
+      throw new Error(`User with idx ${idx} not found`);
+    }
+
+    return user && UserModel.fromPrisma(user);
+  }
+}
