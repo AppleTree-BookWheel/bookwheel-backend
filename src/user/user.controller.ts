70bd9794d525acb373b0b257e51af0c65208e4a0
hj-lab <heeju03180@naver.com>
2025-11-16 16:27:23 +0900

    feat: Add createUser in controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,17 +1,31 @@
-import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe, Post } from '@nestjs/common';
 import { UserService } from './user.service';
+import { CreateUserDto } from './dto/request/create-user.dto';
+import { CreateUserResponseDto } from './dto/response/create-user-response.dto';
+import { GetUserResponseDto } from './dto/response/get-user-response.dto';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get('id/:id')
-  async getUserById(@Param('id') id: string) {
+  async getUserById(
+    @Param('id') id: string,
+  ): Promise<GetUserResponseDto | null> {
     return this.userService.getUserById(id);
   }
 
   @Get('/:idx')
-  async getUserByIdx(@Param('idx', ParseIntPipe) idx: number) {
+  async getUserByIdx(
+    @Param('idx', ParseIntPipe) idx: number,
+  ): Promise<GetUserResponseDto> {
     return this.userService.getUserByIdx(idx);
+  }
+
+  @Post()
+  async createUser(
+    createUserDto: CreateUserDto,
+  ): Promise<CreateUserResponseDto> {
+    return this.userService.createUser(createUserDto);
   }
 }
