38c8d42a6469efb4a72a827da220d5fd8e554f0f
hj-lab <heeju03180@naver.com>
2025-11-19 22:22:07 +0900

    feat: Add createUser, createUserResponse Dto

--- src/api/auth/dto/request/create-user.dto.ts
+++ src/api/auth/dto/request/create-user.dto.ts
@@ -1,5 +1,39 @@
+import {
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  IsInt,
+  ValidateNested,
+  IsEnum,
+} from 'class-validator';
+import { Type } from 'class-transformer';
+import { GenderType } from 'src/api/user/constants/gender-type.enum';
+import { UserType } from 'src/api/user/constants/user-type.enum';
+import { UserBasicDto } from './user-basic.dto';
+
 export class CreateUserDto {
-  id: string;
-  password: string;
-  email: string;
+  @IsOptional()
+  @IsString()
+  public nickname?: string;
+
+  @IsOptional()
+  @IsString()
+  public profileImagePath?: string;
+
+  @IsOptional()
+  @IsInt()
+  public age?: number;
+
+  @IsOptional()
+  @IsEnum(GenderType)
+  public gender?: GenderType;
+
+  @IsNotEmpty()
+  @IsEnum(UserType)
+  public type: UserType;
+
+  @IsNotEmpty()
+  @ValidateNested()
+  @Type(() => UserBasicDto)
+  public basicAuths: UserBasicDto;
 }
