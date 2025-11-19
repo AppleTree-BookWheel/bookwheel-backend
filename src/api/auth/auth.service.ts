7ee1e69cc03192b04ee218e1a5571ea4256aa5da
hj-lab <heeju03180@naver.com>
2025-11-19 22:23:49 +0900

    feat: Add checkDuplicateId and signUp method in service

--- src/api/auth/auth.service.ts
+++ src/api/auth/auth.service.ts
@@ -8,12 +8,16 @@ import { RedisService } from 'src/redis/redis.service';
 import { SendVerificationEmailDto } from './dto/request/send-verification-email.dto';
 import { VerifyCodeDto } from './dto/request/verify-code.dto';
 import { CreateCodeDto } from './dto/request/create-code.dto';
+import { UserRepository } from '../user/user.repository';
+import { CreateUserInput } from './inputs/create-user.input';
+import * as HashUtil from '../../utils/hash.util';
 
 @Injectable()
 export class AuthService {
   constructor(
     private readonly mailerService: MailerService,
     private readonly redisService: RedisService,
+    private readonly userRepository: UserRepository,
   ) {}
 
   // TODO : Transaction 처리 / User 존재 여부 확인 추가
@@ -59,5 +63,31 @@ export class AuthService {
     }
 
     await this.redisService.del(`email_verification:${verifyCodeDto.email}`);
+  }
+
+  public async checkDuplicateId(id: string): Promise<boolean> {
+    const user = this.userRepository.selectUserById(id);
+    return !user; // 존재하지 않으면 true 반환 (사용가능)
+  }
+
+  public async signUp(input: CreateUserInput): Promise<void> {
+    const existId = await this.userRepository.selectUserById(
+      input.basicAuths.id,
+    );
+    if (existId) {
+      throw new BadRequestException('Already existing ID.');
+    }
+
+    const hashedPassword = await HashUtil.hashPassword(
+      input.basicAuths.password,
+    );
+
+    await this.userRepository.insertUser({
+      ...input,
+      basicAuths: {
+        ...input.basicAuths,
+        password: hashedPassword,
+      },
+    });
   }
 }
