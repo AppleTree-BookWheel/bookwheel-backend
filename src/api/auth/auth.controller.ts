586fb53e73d615b4918cf5f8ee6794e678cdfdd7
hj-lab <heeju03180@naver.com>
2025-11-19 22:24:17 +0900

    feat: Add checkDuplicateId and signUp method in controller

--- src/api/auth/auth.controller.ts
+++ src/api/auth/auth.controller.ts
@@ -8,6 +8,7 @@ import {
 import { AuthService } from './auth.service';
 import { SendVerificationEmailDto } from './dto/request/send-verification-email.dto';
 import { VerifyCodeDto } from './dto/request/verify-code.dto';
+import { CreateUserDto } from './dto/request/create-user.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -25,5 +26,17 @@ export class AuthController {
   @UsePipes(ValidationPipe)
   public async verifyCode(@Body() verifyCodeDto: VerifyCodeDto): Promise<void> {
     await this.authService.verifyCode(verifyCodeDto);
+  }
+
+  @Post('check-duplicate-id')
+  @UsePipes(ValidationPipe)
+  public async checkDuplicateId(@Body() id: string): Promise<boolean> {
+    return await this.authService.checkDuplicateId(id);
+  }
+
+  @Post('sign-up')
+  @UsePipes(ValidationPipe)
+  public async signUp(@Body() createUserDto: CreateUserDto): Promise<void> {
+    await this.authService.signUp(createUserDto);
   }
 }
