432e7519df5633270e2dfdf12256aa0394a3cb17
hj-lab <heeju03180@naver.com>
2025-11-23 21:46:14 +0900

    feat: Add jwtmodule in loginToken Module

--- src/api/login-token/login-token.module.ts
+++ src/api/login-token/login-token.module.ts
@@ -1,4 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 
-@Module({})
+@Module({
+  imports: [
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('loginJwt.secret'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+})
 export class LoginTokenModule {}
