1637941304c6332a1c059891f58a105718004e81
hj-lab <heeju03180@naver.com>
2025-11-23 14:49:12 +0900

    feat: Add JwtModule in AuthModule

--- src/api/auth/auth.module.ts
+++ src/api/auth/auth.module.ts
@@ -3,9 +3,13 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { MailerModule } from '@nestjs-modules/mailer';
 import { mailerConfig } from 'src/config/mailer.config';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [MailerModule.forRootAsync(new mailerConfig())],
+  imports: [
+    MailerModule.forRootAsync(new mailerConfig()),
+    JwtModule.register({}),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
