bf1747f284eb57a1258f09efc1ef95da43326548
hj-lab <heeju03180@naver.com>
2025-11-24 19:39:11 +0900

    feat: Add issueAccessToken in service

--- src/api/login-token/login-token.service.ts
+++ src/api/login-token/login-token.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
-export class LoginTokenService {}
+export class LoginTokenService {
+  private readonly ACCESS_TOKEN_EXPIRES_IN: number;
+
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly configService: ConfigService,
+  ) {
+    const config = this.configService.get('loginJwt');
+
+    this.ACCESS_TOKEN_EXPIRES_IN = config.expiresIn;
+  }
+
+  private async issueAccessToken(idx: number, refreshTokenId: string) {
+    const payload = {
+      idx,
+      refreshTokenId,
+    };
+
+    return this.jwtService.sign(payload, {
+      expiresIn: `${this.ACCESS_TOKEN_EXPIRES_IN}m`,
+    });
+  }
+}
