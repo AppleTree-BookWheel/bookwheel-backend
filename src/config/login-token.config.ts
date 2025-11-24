75c4441e0955fc8dffb6e05c820234c3dbb32a19
hj-lab <heeju03180@naver.com>
2025-11-24 19:38:46 +0900

    refactor: Modify loginToken config

--- src/config/login-token.config.ts
+++ src/config/login-token.config.ts
@@ -1,9 +1,9 @@
 export default () => ({
   loginJwt: {
-    secret: process.env.JWT_SECRET,
-    expiresIn: process.env.JWT_EXPIRES_IN,
+    secret: process.env.JWT_ACCESS_SECRET,
+    expiresIn: process.env.JWT_ACCESS_EXPIRES_IN || '15',
 
     refreshSecret: process.env.JWT_REFRESH_SECRET,
-    refreshExpiresIn: process.env.JWT_REFRESH_EXPIRES_IN,
+    refreshExpiresIn: process.env.JWT_REFRESH_EXPIRES_IN || '7',
   },
 });
