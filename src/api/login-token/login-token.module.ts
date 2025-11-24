569b87f51e470ba112f38df3d1fe6e695ca3a26b
hj-lab <heeju03180@naver.com>
2025-11-24 21:49:00 +0900

    feat:Add imports, providers, exports in module

--- src/api/login-token/login-token.module.ts
+++ src/api/login-token/login-token.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
+import { RedisModule } from 'src/redis/redis.module';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { JwtRefreshStrategy } from './strategies/jwt-refresh.strategy';
+import { LoginTokenService } from './login-token.service';
+import { PassportModule } from '@nestjs/passport';
+import loginTokenConfig from 'src/config/login-token.config';
 
 @Module({
   imports: [
+    ConfigModule.forFeature(loginTokenConfig),
+    RedisModule,
+    PassportModule,
     JwtModule.registerAsync({
       imports: [ConfigModule],
       useFactory: (configService: ConfigService) => ({
@@ -11,6 +20,15 @@ import { JwtModule } from '@nestjs/jwt';
       }),
       inject: [ConfigService],
     }),
+  ],
+  providers: [LoginTokenService, JwtStrategy, JwtRefreshStrategy],
+
+  exports: [
+    LoginTokenService,
+    JwtModule,
+    JwtStrategy,
+    JwtRefreshStrategy,
+    PassportModule,
   ],
 })
 export class LoginTokenModule {}
