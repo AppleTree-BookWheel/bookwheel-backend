7de92a61e5cc7a0d715f35978dece0f5e40d29fb
hj-lab <heeju03180@naver.com>
2025-11-27 23:37:12 +0900

    refactor: Modify ReissueTokenSetResponseDto

--- src/api/auth/dto/response/reissue-token-set-response.dto.ts
+++ src/api/auth/dto/response/reissue-token-set-response.dto.ts
@@ -5,18 +5,4 @@ export class ReissueTokenSetResponseDto {
    * @example "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9..."
    */
   accessToken: string;
-
-  /**
-   * 새로 발급된 Refresh Token
-   *
-   * @example "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9..."
-   */
-  refreshToken: string;
-
-  /**
-   * Refresh Token 식별자 (UUID v4)
-   *
-   * @example "550e8400-e29b-41d4-a716-446655440000"
-   */
-  refreshTokenId: string;
 }
