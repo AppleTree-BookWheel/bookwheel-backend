0015e4806f9ecc7a5b20b85699fddb641d90f510
hj-lab <heeju03180@naver.com>
2025-11-29 17:34:32 +0900

    feat: Add getPublicUserById in controller, serivce, repository

--- src/api/user/user.controller.ts
+++ src/api/user/user.controller.ts
@@ -13,17 +13,27 @@ import { UpdateUserDto } from './dto/request/update-user.dto';
 import { UpdatePasswordDto } from './dto/request/update-password.dto';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 import { User } from 'src/common/decorators/user.decorator';
+import { GetPublicUserResponseDto } from './dto/response/get-public-user-response.dto';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  // 로그인시 사용
-  @Post('id')
+  // 로그인, id 중복 검사시 사용
+  @Get('id')
   async getUserById(
     @Body('id') id: string,
   ): Promise<GetUserResponseDto | null> {
     return this.userService.getUserById(id);
+  }
+
+  // id로 친구 검색시 사용
+  @Get('search')
+  @UseGuards(JwtAuthGuard)
+  async getPublicUserById(
+    @Body('id') id: string,
+  ): Promise<GetPublicUserResponseDto | null> {
+    return this.userService.getPublicUserById(id);
   }
 
   @Get()
--- src/api/user/user.service.ts
+++ src/api/user/user.service.ts
@@ -9,12 +9,13 @@ import { CreateUserInput } from './inputs/create-user.input';
 import { UpdateUserInput } from './inputs/update-user.input';
 import { UpdatePasswordInput } from './inputs/update-password.input';
 import * as HashUtil from '../../utils/hash.util';
+import { PublicUserModel } from './model/public-user.model';
 
 @Injectable()
 export class UserService {
   constructor(private readonly userRepository: UserRepository) {}
 
-  // id 중복 검사를 위한 메서드
+  // id 중복 검사, 로그인을 위한 메서드
   public async getUserById(id: string): Promise<UserModel | null> {
     const user = await this.userRepository.selectUserById(id);
 
@@ -23,6 +24,17 @@ export class UserService {
     }
 
     return UserModel.fromPrisma(user);
+  }
+
+  // 친구 검색을 위한 메서드
+  public async getPublicUserById(id: string): Promise<PublicUserModel | null> {
+    const user = await this.userRepository.selectPublicUserById(id);
+
+    if (!user) {
+      return null;
+    }
+
+    return PublicUserModel.fromPrisma(user);
   }
 
   public async getUserByIdx(idx: number): Promise<UserModel> {
