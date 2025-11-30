dce4827940b3e48a3641f682963c0f63a76b6f70
hj-lab <heeju03180@naver.com>
2025-11-30 15:45:39 +0900

    feat: Initial setting

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,6 +12,7 @@ import { SurveyModule } from './api/survey/survey.module';
 import { UserModule } from './api/user/user.module';
 import { LoginTokenModule } from './api/login-token/login-token.module';
 import { AuthModule } from './api/auth/auth.module';
+import { BookModule } from './api/book/book.module';
 
 @Module({
   imports: [
@@ -33,6 +34,7 @@ import { AuthModule } from './api/auth/auth.module';
     UserModule,
     LoginTokenModule,
     AuthModule,
+    BookModule,
   ],
   controllers: [AppController],
   providers: [AppService],
