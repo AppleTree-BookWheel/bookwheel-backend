1f05200726b9e365f58640816dce0fd40dc4f00d
hj-lab <heeju03180@naver.com>
2025-11-30 21:04:25 +0900

    feat: Add get methods in controller

--- src/api/book/book.controller.ts
+++ src/api/book/book.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { BookService } from './book.service';
+import { GetBookOverviewResponseDto } from './dto/response/get-book-overview-response.dto';
+import { GetBookResponseDto } from './dto/response/get-book-response.dto';
 
 @Controller('book')
-export class BookController {}
+export class BookController {
+  constructor(private readonly bookService: BookService) {}
+
+  @Get('overview')
+  async getBookOverviewsByIdx(
+    idx: number[],
+  ): Promise<GetBookOverviewResponseDto[]> {
+    return this.bookService.getBookOverviewsByIdx(idx);
+  }
+
+  @Get()
+  async getBooksByIdx(idx: number[]): Promise<GetBookResponseDto[]> {
+    return this.bookService.getBooksByIdx(idx);
+  }
+}
