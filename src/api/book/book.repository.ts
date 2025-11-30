83fc253308311259f157eb642fdceabd12777396
hj-lab <heeju03180@naver.com>
2025-11-30 17:35:11 +0900

    feat: Add selectBookOverviewsByIdx and selectBooksByIdx in repository

--- src/api/book/book.repository.ts
+++ src/api/book/book.repository.ts
@@ -1,10 +1,49 @@
 import { TransactionHost } from '@nestjs-cls/transactional';
 import { TransactionalAdapterPrisma } from '@nestjs-cls/transactional-adapter-prisma';
 import { Injectable } from '@nestjs/common';
+import {
+  SELECT_BOOK_OVERVIEW,
+  SelectBookOverview,
+} from './model/prisma-type/select-book-overview';
+import { SELECT_BOOK, SelectBook } from './model/prisma-type/select-book';
 
 @Injectable()
 export class BookRepository {
   constructor(
     private readonly txHost: TransactionHost<TransactionalAdapterPrisma>,
   ) {}
+
+  async selectBookOverviewsByIdx(
+    idx: number[],
+  ): Promise<SelectBookOverview[] | null> {
+    if (idx.length === 0) {
+      return [];
+    }
+
+    return await this.txHost.tx.book.findMany({
+      ...SELECT_BOOK_OVERVIEW,
+      where: {
+        idx: {
+          in: idx,
+        },
+        deletedAt: null,
+      },
+    });
+  }
+
+  async selectBooksByIdx(idx: number[]): Promise<SelectBook[] | null> {
+    if (idx.length === 0) {
+      return [];
+    }
+
+    return await this.txHost.tx.book.findMany({
+      ...SELECT_BOOK,
+      where: {
+        idx: {
+          in: idx,
+        },
+        deletedAt: null,
+      },
+    });
+  }
 }
