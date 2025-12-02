9a140db4fbe5dbb733f4536760b382eaec159657
hj-lab <heeju03180@naver.com>
2025-12-02 21:34:42 +0900

    feat: Add selectHighlightsByPartyIdx in service

--- src/api/book-highlight/book-highlight.service.ts
+++ src/api/book-highlight/book-highlight.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { BookHighlightModel } from './model/book-highlight.model';
+import { BookHighlightRepository } from './book-highlight.repository';
 
 @Injectable()
-export class BookHighlightService {}
+export class BookHighlightService {
+  constructor(
+    private readonly bookHighlightRepository: BookHighlightRepository,
+  ) {}
+
+  public async getHighlightsByPartyIdx(
+    partyIdx: number,
+  ): Promise<BookHighlightModel[]> {
+    return await this.bookHighlightRepository.selectHighlightsByPartyIdx(
+      partyIdx,
+    );
+  }
+}
