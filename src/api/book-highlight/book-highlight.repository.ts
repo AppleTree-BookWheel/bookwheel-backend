7ba906b3252ba977ee626e54d8e732294b65c486
hj-lab <heeju03180@naver.com>
2025-12-02 21:34:19 +0900

    feat: Add selectHighlightsByPartyIdx in repository

--- src/api/book-highlight/book-highlight.repository.ts
+++ src/api/book-highlight/book-highlight.repository.ts
@@ -1,6 +1,29 @@
+import { TransactionHost } from '@nestjs-cls/transactional';
+import { TransactionalAdapterPrisma } from '@nestjs-cls/transactional-adapter-prisma';
 import { Injectable } from '@nestjs/common';
+import {
+  SELECT_BOOK_HIGHLIGHT,
+  SelectBookHighlight,
+} from './model/prisma-type/select-book-highlight';
 
 @Injectable()
 export class BookHighlightRepository {
-  constructor() {}
+  constructor(
+    private readonly txHost: TransactionHost<TransactionalAdapterPrisma>,
+  ) {}
+
+  public async selectHighlightsByPartyIdx(
+    partyIdx: number,
+  ): Promise<SelectBookHighlight[]> {
+    return await this.txHost.tx.bookHighlight.findMany({
+      ...SELECT_BOOK_HIGHLIGHT,
+      where: {
+        partyIdx,
+        deletedAt: null,
+      },
+      orderBy: {
+        createdAt: 'asc',
+      },
+    });
+  }
 }
