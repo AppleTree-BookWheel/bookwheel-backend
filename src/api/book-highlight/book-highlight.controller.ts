42bc8fcbe512415b2af3e62bdb3ddae735db287b
hj-lab <heeju03180@naver.com>
2025-12-04 14:51:49 +0900

    feat: Add getMyHighlights in controller

--- src/api/book-highlight/book-highlight.controller.ts
+++ src/api/book-highlight/book-highlight.controller.ts
@@ -2,6 +2,8 @@ import { Body, Controller, Get, UseGuards } from '@nestjs/common';
 import { BookHighlightService } from './book-highlight.service';
 import { GetBookHighlightResponseDto } from './dto/request/get-book-highlight-response.dto';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { User } from 'src/common/decorators/user.decorator';
+import { GetMyHighlightResponseDto } from './dto/response/get-my-highlight-response.dto';
 
 @UseGuards(JwtAuthGuard)
 @Controller('book-highlight')
@@ -13,5 +15,17 @@ export class BookHighlightController {
     @Body() partyIdx: number,
   ): Promise<GetBookHighlightResponseDto[]> {
     return await this.bookHighlightService.getHighlightsByPartyIdx(partyIdx);
+  }
+
+  // 내 하이라이트 모음 페이지를 위한 API
+  @Get()
+  public async getMyHighlight(
+    @User() user,
+    @Body('partyIdx') partyIdx: number,
+  ): Promise<GetMyHighlightResponseDto[]> {
+    return await this.bookHighlightService.getMyHighlights({
+      partyIdx,
+      userIdx: user.idx,
+    });
   }
 }
