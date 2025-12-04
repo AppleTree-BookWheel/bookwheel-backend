b65fc7646cce8021dd8a44293bd94e153637c903
hj-lab <heeju03180@naver.com>
2025-12-04 14:51:31 +0900

    feat: Add getMyHighlights in service

--- src/api/book-highlight/book-highlight.service.ts
+++ src/api/book-highlight/book-highlight.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { HighlightModel } from './model/highlight.model';
 import { BookHighlightRepository } from './book-highlight.repository';
+import { MyHighlightModel } from './model/my-highlight.model';
+import { getMyHighlightInput } from './inputs/get-my-highlight.input';
 
 @Injectable()
 export class BookHighlightService {
@@ -14,5 +16,15 @@ export class BookHighlightService {
     return await this.bookHighlightRepository.selectHighlightsByPartyIdx(
       partyIdx,
     );
+  }
+
+  public async getMyHighlights(
+    input: getMyHighlightInput,
+  ): Promise<MyHighlightModel[]> {
+    const response =
+      await this.bookHighlightRepository.selectHighlightsByPartyAndUserIdx(
+        input,
+      );
+    return response.map((data) => MyHighlightModel.fromPrisma(data));
   }
 }
