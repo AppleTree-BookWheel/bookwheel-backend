fd8c7951241a2498407641ac55a80655929de2fe
hj-lab <heeju03180@naver.com>
2025-12-04 15:53:51 +0900

    feat: Add createHighlightDto and Modify GetBookHighlightResponseDto

--- src/api/book-highlight/dto/request/create-highlight.dto.ts
+++ src/api/book-highlight/dto/request/create-highlight.dto.ts
@@ -1,27 +1,27 @@
-export class GetBookHighlightResponseDto {
-  /**
-   * 하이라이트 식별자
-   *
-   * @example 1
-   */
-  idx: number;
-
-  /**
-   * 작성자 식별자
-   *
-   * @example 10
-   */
-  userIdx: number;
-
+export class CreateHighlightDto {
   /**
    * 파티 식별자
    *
-   * @example 5
+   * @example 1
    */
   partyIdx: number;
 
   /**
-   * 전자책 내 하이라이트 위치 정보 (CFI)
+   * 책 식별자
+   *
+   * @example 2
+   */
+  bookIdx: number;
+
+  /**
+   * 작성자(유저) 식별자
+   *
+   * @example 3
+   */
+  userIdx: number;
+
+  /**
+   * 하이라이트 위치 정보 (CFI)
    *
    * @example "epubcfi(/6/4[chap01ref]!/4/2/1:0)"
    */
@@ -40,11 +40,4 @@ export class GetBookHighlightResponseDto {
    * @example "#FF5733"
    */
   colorCode: string;
-
-  /**
-   * 생성 일시
-   *
-   * @example "2023-10-25T12:00:00.000Z"
-   */
-  createdAt: Date;
 }
