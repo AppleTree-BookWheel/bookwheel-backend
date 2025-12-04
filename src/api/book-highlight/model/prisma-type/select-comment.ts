8e20399206b4684210599088e3db9357d99347e9
hj-lab <heeju03180@naver.com>
2025-12-04 21:12:41 +0900

    feat: Add prisma-type and model for comment

---
+++ src/api/book-highlight/model/prisma-type/select-comment.ts
@@ -0,0 +1,21 @@
+import { Prisma } from '@prisma/client';
+
+export const SELECT_COMMENT = Prisma.validator<Prisma.BookCommentDefaultArgs>()(
+  {
+    select: {
+      idx: true,
+      userIdx: true,
+      highlightIdx: true,
+      content: true,
+      createdAt: true,
+      user: {
+        select: {
+          nickname: true,
+          profileImagePath: true,
+        },
+      },
+    },
+  },
+);
+
+export type SelectComment = Prisma.BookCommentGetPayload<typeof SELECT_COMMENT>;
