60b30c1c900e29623bad237f327f8d7eceff3e2d
hj-lab <heeju03180@naver.com>
2025-12-05 21:34:11 +0900

    feat: Add createParty in controller

--- src/api/party/party.controller.ts
+++ src/api/party/party.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { PartyService } from './party.service';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { CreatePartyDto } from './dto/request/create-party.dto';
+import { User } from 'src/common/decorators/user.decorator';
 
+@UseGuards(JwtAuthGuard)
 @Controller('party')
-export class PartyController {}
+export class PartyController {
+  constructor(private readonly partyService: PartyService) {}
+
+  @Post()
+  public async createParty(
+    @User() user,
+    @Body() createPartyDto: CreatePartyDto,
+  ): Promise<void> {
+    await this.partyService.createParty(user.idx, createPartyDto);
+  }
+}
