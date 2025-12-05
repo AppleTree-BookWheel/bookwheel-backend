cd6a450d3d02fe0a148ed40a8edb57bfdb9a7837
hj-lab <heeju03180@naver.com>
2025-12-05 21:01:52 +0900

    feat: Add insertParty in repoository

--- src/api/party/party.repository.ts
+++ src/api/party/party.repository.ts
@@ -1,10 +1,42 @@
 import { TransactionHost } from '@nestjs-cls/transactional';
 import { TransactionalAdapterPrisma } from '@nestjs-cls/transactional-adapter-prisma';
 import { Injectable } from '@nestjs/common';
+import { CreatePartyInput } from './inputs/create-party.input';
+import { SELECT_PARTY, SelectParty } from './model/prisma-type/select-party';
+import { PartyStatus } from './constants/party-status';
+import { PartyMemberStatus } from './constants/party-member-status';
 
 @Injectable()
 export class PartyRepository {
   constructor(
     private readonly txHost: TransactionHost<TransactionalAdapterPrisma>,
   ) {}
+
+  public async insertParty(
+    hostUserIdx: number,
+    input: CreatePartyInput,
+  ): Promise<SelectParty> {
+    return await this.txHost.tx.party.create({
+      ...SELECT_PARTY,
+      data: {
+        hostUserIdx,
+        bookIdx: input.bookIdx,
+        title: input.title,
+        description: input.description,
+        maxMembers: input.maxMembers,
+        currentMembers: 1,
+        status: PartyStatus.OPEN,
+        startDate: input.startDate,
+        isPrivate: input.isPrivate,
+        password: input.password ?? null,
+
+        memberships: {
+          create: {
+            userIdx: hostUserIdx,
+            status: PartyMemberStatus.ACCEPTED,
+          },
+        },
+      },
+    });
+  }
 }
