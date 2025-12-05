77b9d7fff40d2a88c04fd6ed494bfdce83ffa5d7
hj-lab <heeju03180@naver.com>
2025-12-05 21:34:03 +0900

    feat: Add createParty in service

--- src/api/party/party.service.ts
+++ src/api/party/party.service.ts
@@ -1,4 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { PartyRepository } from './party.repository';
+import { CreatePartyInput } from './inputs/create-party.input';
+import { ExceptionsHandler } from '@nestjs/core/exceptions/exceptions-handler';
 
 @Injectable()
-export class PartyService {}
+export class PartyService {
+  constructor(private readonly partyRepository: PartyRepository) {}
+
+  public async createParty(
+    hostUserIdx: number,
+    input: CreatePartyInput,
+  ): Promise<void> {
+    const { invitedUserIdxs, ...partyData } = input;
+
+    if (partyData.isPrivate && !partyData.password) {
+      throw new BadRequestException(
+        'Password is required for private parties.',
+      );
+    }
+
+    await this.partyRepository.insertParty(hostUserIdx, partyData);
+
+    if (invitedUserIdxs && invitedUserIdxs.length > 0) {
+      // TODO : 유저 초대 message 기능 추가 예정
+    }
+  }
+}
