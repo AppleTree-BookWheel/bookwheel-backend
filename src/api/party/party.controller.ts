9174ea9987465506779255e4af0e4cef8e488688
hj-lab <heeju03180@naver.com>
2025-12-07 01:15:11 +0900

    feat: Add updatePartyByUserAndPartyIdx in controller

--- src/api/party/party.controller.ts
+++ src/api/party/party.controller.ts
@@ -1,10 +1,11 @@
-import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Patch, Post, UseGuards } from '@nestjs/common';
 import { PartyService } from './party.service';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 import { CreatePartyDto } from './dto/request/create-party.dto';
 import { User } from 'src/common/decorators/user.decorator';
 import { GetPartyOverviewResponseDto } from './dto/response/get-party-overview-response.dto';
 import { GetPartyResponseDto } from './dto/response/get-party-response.dto';
+import { UpdatePartyDto } from './dto/request/update-party.dto';
 
 @UseGuards(JwtAuthGuard)
 @Controller('party')
@@ -29,5 +30,16 @@ export class PartyController {
     @Body('partyIdx') partyIdx: number,
   ): Promise<GetPartyResponseDto | null> {
     return await this.partyService.getPartyByIdx(partyIdx);
+  }
+
+  @Patch()
+  public async updatePartyByUserAndPartyIdx(
+    @User() user,
+    @Body() updatePartyDto: UpdatePartyDto,
+  ): Promise<void> {
+    await this.partyService.updatePartyByUserAndPartyIdx(
+      user.idx,
+      updatePartyDto,
+    );
   }
 }
