5cafd5d610bee56a8780ef1aabb17ad96b3e0f36
JEONG-YEON-KANG <kjykjy3314@naver.com>
2025-12-07 16:37:38 +0900

    feat: Finish recommend repository

--- src/api/recommend/recommend.repository.ts
+++ src/api/recommend/recommend.repository.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
-
+import axios from 'axios';
 @Injectable()
-export class RecommendRepository {}
+export class RecommendRepository {
+  private readonly BASE_URL = process.env.RECOMMEND_API_URL;
+
+  async getHomeRecommend(userIdx: number) {
+    const res = await axios.get(`${this.BASE_URL}/home`, {
+      headers: { 'X-User-Idx': userIdx },
+    });
+    return res.data;
+  }
+
+  async getSimilarBooksRecommend(bookIdx: number) {
+    const res = await axios.get(`${this.BASE_URL}/books/${bookIdx}`);
+    return res.data;
+  }
+
+  async getSimilarUsersRecommend(userIdx: number) {
+    const res = await axios.get(`${this.BASE_URL}/user/${userIdx}`);
+    return res.data;
+  }
+}
