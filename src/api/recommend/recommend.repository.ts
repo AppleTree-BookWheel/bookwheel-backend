e1e1533720c38011121dec22b86cf8f681093887
JEONG-YEON-KANG <kjykjy3314@naver.com>
2025-12-07 20:27:04 +0900

    feat: Add interface to repository

--- src/api/recommend/recommend.repository.ts
+++ src/api/recommend/recommend.repository.ts
@@ -1,22 +1,32 @@
 import { Injectable } from '@nestjs/common';
 import axios from 'axios';
+import { HomeResponseInterface } from './interface/home-response.interface';
+import { BookListResponseInterface } from './interface/book-list-response.interface';
+import { UserListResponseInterface } from './interface/user-list-response.interface';
 @Injectable()
 export class RecommendRepository {
   private readonly BASE_URL = process.env.RECOMMEND_API_URL;
 
-  async getHomeRecommend(userIdx: number) {
-    const res = await axios.get(`${this.BASE_URL}/home`, {
-      headers: { 'X-User-Idx': userIdx },
-    });
+  async getHomeRecommend(userIdx: number): Promise<HomeResponseInterface> {
+    const res = await axios.get<HomeResponseInterface>(
+      `${this.BASE_URL}/home`,
+      {
+        headers: { 'X-User-Idx': userIdx },
+      },
+    );
     return res.data;
   }
 
-  async getSimilarBooksRecommend(bookIdx: number) {
+  async getSimilarBooksRecommend(
+    bookIdx: number,
+  ): Promise<BookListResponseInterface> {
     const res = await axios.get(`${this.BASE_URL}/books/${bookIdx}`);
     return res.data;
   }
 
-  async getSimilarUsersRecommend(userIdx: number) {
+  async getSimilarUsersRecommend(
+    userIdx: number,
+  ): Promise<UserListResponseInterface> {
     const res = await axios.get(`${this.BASE_URL}/user/${userIdx}`);
     return res.data;
   }
