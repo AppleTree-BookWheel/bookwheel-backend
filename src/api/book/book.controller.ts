dd88e5e3ed246f3522a5141fa6ec8e5d38c7d877
hj-lab <heeju03180@naver.com>
2025-12-09 00:57:46 +0900

    feat: Add updateMyBookProgressByUserAndBookIdx in controller

--- src/api/book/book.controller.ts
+++ src/api/book/book.controller.ts
@@ -1,10 +1,12 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Patch, Post } from '@nestjs/common';
 import { BookService } from './book.service';
 import { GetBookOverviewResponseDto } from './dto/response/get-book-overview-response.dto';
 import { GetBookResponseDto } from './dto/response/get-book-response.dto';
 import { GetBookOverviewDto } from './dto/request/get-book-overview.dto';
 import { GetBookDto } from './dto/request/get-book.dto';
 import { GetSearchBookDto } from './dto/request/get-search-book.dto';
+import { UpdateMyBookDto } from './dto/request/update-my-book.dto';
+import { User } from 'src/common/decorators/user.decorator';
 
 @Controller('book')
 export class BookController {
@@ -29,5 +31,13 @@ export class BookController {
     @Body() getSearchBookDto: GetSearchBookDto,
   ): Promise<GetBookResponseDto[]> {
     return this.bookService.getBooksByKeyword(getSearchBookDto.keyword);
+  }
+
+  @Patch('/my-progress')
+  async updateMyBookProgressByUserAndBookIdx(
+    @User() user,
+    @Body() dto: UpdateMyBookDto,
+  ): Promise<void> {
+    return this.bookService.updateMyBookProgressByUserAndBookIdx(user.idx, dto);
   }
 }
