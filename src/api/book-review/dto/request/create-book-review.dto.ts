10e94bda025765e2dac5ccc834435ab8ff6967c1
hj-lab <heeju03180@naver.com>
2025-12-08 14:04:49 +0900

    feat: Add create/get bookReview DTOs

---
+++ src/api/book-review/dto/request/create-book-review.dto.ts
@@ -0,0 +1,29 @@
+import { IsInt, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
+export class CreateBookReviewDto {
+  /**
+   * 책 고유 idx
+   *
+   * @example 1
+   */
+  @IsNotEmpty()
+  @IsInt()
+  bookIdx: number;
+
+  /**
+   * 평점
+   *
+   * @example 5
+   */
+  @IsNotEmpty()
+  rating: number;
+
+  /**
+   * 리뷰 내용
+   *
+   * @example "정말 재미있는 책이었어요!"
+   */
+  @IsOptional()
+  @IsString()
+  content?: string;
+}
