7f9b4bc8e443069dd8b83d0e7f2708be47aff4e3
hj-lab <heeju03180@naver.com>
2025-12-08 14:05:31 +0900

    feat: Add create/get BookReview in service

--- src/api/book-review/book-review.service.ts
+++ src/api/book-review/book-review.service.ts
@@ -1,4 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { BookReviewRepository } from './book-review.repository';
+import { CreateBookReviewInput } from './inputs/create-book-review.input';
+import { BookReviewModel } from './model/book-review.model';
 
 @Injectable()
-export class BookReviewService {}
+export class BookReviewService {
+  constructor(private readonly bookReviewRepository: BookReviewRepository) {}
+
+  public async createBookReview(
+    userIdx: number,
+    input: CreateBookReviewInput,
+  ): Promise<void> {
+    if (input.rating < 0 || input.rating > 5) {
+      throw new BadRequestException('Rating must be between 0 and 5.');
+    }
+
+    return this.bookReviewRepository.insertBookReview(userIdx, input);
+  }
+
+  public async getBookReviewByUserAndBookIdx(
+    userIdx: number,
+    bookIdx: number,
+  ): Promise<BookReviewModel | null> {
+    return this.bookReviewRepository.selectBookReviewByUserAndBookIdx(
+      userIdx,
+      bookIdx,
+    );
+  }
+}
