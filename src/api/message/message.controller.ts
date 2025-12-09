ac96abc3124f32e976bfd89dcf1bc0b10306ae35
hj-lab <heeju03180@naver.com>
2025-12-09 17:56:18 +0900

    feat: Add createMessage in controller

--- src/api/message/message.controller.ts
+++ src/api/message/message.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { MessageService } from './message.service';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { CreateMessageDto } from './dto/request/create-message.dto';
+import { User } from 'src/common/decorators/user.decorator';
 
+@UseGuards(JwtAuthGuard)
 @Controller('message')
-export class MessageController {}
+export class MessageController {
+  constructor(private readonly messageService: MessageService) {}
+
+  @Post()
+  public async createMessage(
+    @User() user,
+    @Body() dto: CreateMessageDto,
+  ): Promise<void> {
+    await this.messageService.createMessage(user.idx, dto);
+  }
+}
