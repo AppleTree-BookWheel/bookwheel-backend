2aca68f4eae260e220f1302a144cd9a49fcfc1a4
hj-lab <heeju03180@naver.com>
2025-12-09 17:55:44 +0900

    feat: Add createMessage in service

--- src/api/message/message.service.ts
+++ src/api/message/message.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { MessageRepository } from './message.repository';
+import { CreateMessageInput } from './inputs/create-message.input';
 
 @Injectable()
-export class MessageService {}
+export class MessageService {
+  constructor(private readonly messageRepository: MessageRepository) {}
+
+  public async createMessage(
+    senderIdx: number,
+    input: CreateMessageInput,
+  ): Promise<void> {
+    await this.messageRepository.insertMessage(senderIdx, input);
+  }
+}
