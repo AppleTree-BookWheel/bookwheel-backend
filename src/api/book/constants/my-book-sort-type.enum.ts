e28d4955f800c34f323685958f13c43549be540d
hj-lab <heeju03180@naver.com>
2025-12-10 02:14:24 +0900

    feat: Add MyBookSortType enum

---
+++ src/api/book/constants/my-book-sort-type.enum.ts
@@ -0,0 +1,6 @@
+export enum MyBookSortType {
+  RECENTLY_VIEWED = 'RECENTLY_VIEWED', // 최근 본 순 (Default)
+  TITLE = 'TITLE', // 책 제목 순
+  AUTHOR = 'AUTHOR', // 저자명 순
+  PUBLICATION = 'PUBLICATION', // 최근 발행 순 (출판일)
+}
