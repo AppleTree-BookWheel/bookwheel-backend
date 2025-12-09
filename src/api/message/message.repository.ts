28df8ff5caa84dd56118f818e188514f84a275df
hj-lab <heeju03180@naver.com>
2025-12-09 17:53:22 +0900

    feat: Add insertMessage in repository

--- src/api/message/message.repository.ts
+++ src/api/message/message.repository.ts
@@ -1,10 +1,30 @@
 import { TransactionHost } from '@nestjs-cls/transactional';
 import { TransactionalAdapterPrisma } from '@nestjs-cls/transactional-adapter-prisma';
 import { Injectable } from '@nestjs/common';
+import { CreateMessageInput } from './inputs/create-message.input';
+import {
+  SELECT_MESSAGE,
+  SelectMessage,
+} from './model/prisma-type/select-message';
 
 @Injectable()
 export class MessageRepository {
   constructor(
     private readonly txHost: TransactionHost<TransactionalAdapterPrisma>,
   ) {}
+
+  public async insertMessage(
+    senderIdx: number,
+    input: CreateMessageInput,
+  ): Promise<SelectMessage> {
+    return await this.txHost.tx.message.create({
+      ...SELECT_MESSAGE,
+      data: {
+        senderIdx: senderIdx,
+        receiverIdx: input.receiverIdx,
+        content: input.content,
+        isRead: false,
+      },
+    });
+  }
 }
