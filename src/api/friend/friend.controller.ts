f934c5012bca07ec91d7232fbd7cfca09de4819e
hj-lab <heeju03180@naver.com>
2025-12-13 15:40:16 +0900

    feat: Add getFriendsByUserIdx in controller

--- src/api/friend/friend.controller.ts
+++ src/api/friend/friend.controller.ts
@@ -3,12 +3,14 @@ import { FriendService } from './friend.service';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 import { User } from 'src/common/decorators/user.decorator';
 import { FriendOverviewModel } from './model/friend-overview.model';
+import { GetFriendOverviewResponseDto } from './dto/response/get-friend-overview-response.dto';
 
 @UseGuards(JwtAuthGuard)
 @Controller('friend')
 export class FriendController {
   constructor(private readonly friendService: FriendService) {}
 
+  // 친구 요청 보내기
   @Post('/request/:receiveUserIdx')
   public async createFriendRequest(
     @User() user,
@@ -17,10 +19,19 @@ export class FriendController {
     await this.friendService.createFriendRequest(user.idx, receiveUserIdx);
   }
 
+  // 받은 친구 요청 목록 조회
   @Get('/requests')
   public async getReceivedFriendRequestsByUserIdx(
     @User() user,
-  ): Promise<FriendOverviewModel[]> {
+  ): Promise<GetFriendOverviewResponseDto[]> {
     return this.friendService.getReceivedFriendRequestsByUserIdx(user.idx);
+  }
+
+  // 친구 목록 조회
+  @Get('')
+  public async getFriendsByUserIdx(
+    @User() user,
+  ): Promise<GetFriendOverviewResponseDto[]> {
+    return this.friendService.getFriendsByUserIdx(user.idx);
   }
 }
