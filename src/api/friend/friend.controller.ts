e9860a690bd3dd73590713b9380be4acad8b9eb3
hj-lab <heeju03180@naver.com>
2025-12-13 15:05:41 +0900

    feat: Add createFriendRequest in controller

--- src/api/friend/friend.controller.ts
+++ src/api/friend/friend.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Param, Post, UseGuards } from '@nestjs/common';
+import { FriendService } from './friend.service';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { User } from 'src/common/decorators/user.decorator';
 
+@UseGuards(JwtAuthGuard)
 @Controller('friend')
-export class FriendController {}
+export class FriendController {
+  constructor(private readonly friendService: FriendService) {}
+
+  @Post('/request/:receiveUserIdx')
+  public async createFriendRequest(
+    @User() user,
+    @Param('receiveUserIdx') receiveUserIdx: number,
+  ): Promise<void> {
+    await this.friendService.createFriendRequest(user.idx, receiveUserIdx);
+  }
+}
